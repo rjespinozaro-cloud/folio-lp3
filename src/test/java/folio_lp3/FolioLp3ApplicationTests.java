1c8a5a4e25db122eaa44bc476e1be620358617d9
Jo4nlu <rj.espinozaro@unsm.edu.pe>
2026-05-26 19:34:56 -0500

    feat: estructura inicial de Spring Boot e instrucciones de ciberseguridad para MariaDB

---
+++ src/test/java/folio_lp3/FolioLp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package folio_lp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolioLp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
